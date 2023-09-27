d4c93016488502d194e844b32d8736406e95186c
askar13 <86620613+askar13@users.noreply.github.com>
2023-09-27 13:08:49 -0300

    Update Teste.java

--- Teste.java
+++ Teste.java
@@ -1,41 +1,87 @@
 package Televisao;
 
-public class TV {
-	private int volume, canal;
-	String marca;
-	boolean ligado;
+import java.util.Scanner;
 
-	public TV(int volume, int canal) {
-		this.volume = volume;
-		this.canal = canal;
-		this.marca = "";
-		this.ligado = false;
-	}
-	
-	 public int getVolume() {
-		 return volume;
-	 }
+public class Teste {
+	public static void main(String[] args) {
+		ControleRemoto controle = new ControleRemoto();
+		Scanner teclado = new Scanner(System.in);
+		
+		TV tv = new TV(0, 0);
+		
+		int seleciona;
+	    do {
+	     	System.out.printf("\n%15s\n\n","Controle");
+	        System.out.println("1>> Ligar/Desligar");
+	        System.out.println("2>> Aumentar volume");
+	        System.out.println("3>> Diminuir volume");
+	        System.out.println("4>> Aumentar canal");
+	        System.out.println("5>> Diminuir canal");
+	        System.out.println("6>> Encerrar programa");
+	        System.out.printf("\nSelecione uma opção>> ");
+	        seleciona = teclado.nextInt();
+	        
+	        switch (seleciona){
+	        	case 1:
+	        		if (!tv.getLigado()) {
+	        			tv.setLigado(true);
+	                	System.out.println("\nSua televisão está ligada.");
+	                	break;
+	                } else if (tv.getLigado()){
+	                	tv.setLigado(false);
+	                	System.out.println("\nSua televisão está desligada.");
+	                }
+	        		break;
+	        		
+	            case 2:
+	            if (!tv.getLigado()) {
+		            	System.out.println("\nSua televisão está desligada.");
 
-	public void setVolume(int volume) {
-		this.volume = volume;
-	}
-	
-	public int getCanal() {
-		return canal;
-	}
-	
-	public void setCanal(int canal) {
-		this.canal = canal;
-	}
+		            } else if (tv.getLigado()) {
+		            	controle.aumentarVolume();
+		            	System.out.printf("Volume atual é %d", tv.getVolume());
+			        }    
+		            break;
+		            
+	            case 3:
+	            	if (!tv.getLigado()) {
+	            		System.out.println("\nSua televisão está desligada.");
 
-	public boolean getLigado() {
-		return ligado;
+		            } else if (tv.getLigado()) {
+		            	controle.diminuirVolume();
+		            	System.out.printf("Volume atual é %d", tv.getVolume());
+		            }	
+		            break;
+		            
+	            case 4:
+	            	if (!tv.getLigado()) {
+	            		System.out.println("\nSua televisão está desligada.");
+	
+            	} else if (tv.getLigado()) {
+	            		controle.aumentarCanal();
+	            		System.out.printf("Canal atual é %d", tv.getCanal());
+					}
+	            	break;
+	            	
+	            case 5:
+	            	if (!tv.getLigado()) {
+	            		System.out.println("\nSua televisão está desligada.");
+
+	            	} else if (tv.getLigado()) {
+            		controle.diminuirCanal();
+            		System.out.printf("Canal atual é %d", tv.getCanal());
+	            	}
+	            	break;
+	            	
+	            case 6:
+            	System.out.println("\nPrograma finalizado. Até a próxima");
+                  break;
+                  
+	            default:
+	            	System.out.println("\nVocê selecionou uma opção inválida.");
+	        }
+        } while (seleciona != 6);
+    
+	    teclado.close();
 	}
-	
-	public void setLigado(boolean ligado) {
-		this.ligado = ligado;
-	}
-	
-	
-	
 }
